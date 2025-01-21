d610ee39259c59ffc4698787665cefd1bd7d5286
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-01-21 16:50:14 -0300

    incluido paginas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,25 @@
-import { Routes } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { EmpresaComponent } from './empresa/empresa.component';
+import { ContatoComponent } from './contato/contato.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { NgModule } from '@angular/core';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: "", component: HomeComponent},
+    { path: "/produtos", component: ProdutosComponent},
+    { path: "/clientes", component: ClientesComponent},
+    { path: "/empresa", component: EmpresaComponent},
+    { path: "/contato", component: ContatoComponent},
+    { path: "/carrinho", component: CarrinhoComponent}
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule],
+})
+
+export class AppRoutingModule {}
