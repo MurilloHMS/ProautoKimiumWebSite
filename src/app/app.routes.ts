273e259532e9eab098ccd4b404cfc4c2d1102aab
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-01-21 17:18:50 -0300

    finalizado rotas e criado pagina de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,15 +6,17 @@ import { EmpresaComponent } from './empresa/empresa.component';
 import { ContatoComponent } from './contato/contato.component';
 import { CarrinhoComponent } from './carrinho/carrinho.component';
 import { NgModule } from '@angular/core';
+import { LoginComponent } from './login/login.component';
 
 
 export const routes: Routes = [
     { path: "", component: HomeComponent},
-    { path: "/produtos", component: ProdutosComponent},
-    { path: "/clientes", component: ClientesComponent},
-    { path: "/empresa", component: EmpresaComponent},
-    { path: "/contato", component: ContatoComponent},
-    { path: "/carrinho", component: CarrinhoComponent}
+    { path: "produtos", component: ProdutosComponent},
+    { path: "clientes", component: ClientesComponent},
+    { path: "empresa", component: EmpresaComponent},
+    { path: "contato", component: ContatoComponent},
+    { path: "carrinho", component: CarrinhoComponent},
+    { path: "login", component: LoginComponent}
 ];
 
 @NgModule({
