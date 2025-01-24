263e1ec58335a845d1e586e93aec465cc28d8203
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-01-24 16:27:35 -0300

    melhoria no design dos produtos

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ProdutosComponent } from "../produtos/produtos.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [ProdutosComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
