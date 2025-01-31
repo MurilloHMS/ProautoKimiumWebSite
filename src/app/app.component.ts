7325c91a9354a61cfa96434ea49f37d7383a0782
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-01-31 09:13:04 -0300

    incluido scroll to top
    criado componente para incluir scroll to top em todas as páginas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./shared/header/header.component";
 import { FooterComponent } from "./shared/footer/footer.component";
+import { ScrollToTopComponent } from "./Components/scroll-to-top/scroll-to-top.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, ScrollToTopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
