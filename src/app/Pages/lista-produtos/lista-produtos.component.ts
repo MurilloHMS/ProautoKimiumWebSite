3425b06c16f66dd982a0b0555a2075f9de1a1103
MurilloHMS <murillohenriquemelo@outlook.com>
2025-02-13 16:34:12 -0300

    feat: componente snackbar
    criado componente snackbar para substituir o alert

--- src/app/Pages/lista-produtos/lista-produtos.component.ts
+++ src/app/Pages/lista-produtos/lista-produtos.component.ts
@@ -3,11 +3,12 @@ import { categorias, produtos } from '../../../Type/produtos';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { CartService } from '../../services/cart.service';
+import { SnackbarComponent } from '../../Components/snackbar/snackbar.component';
 
 @Component({
   selector: 'app-lista-produtos',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, SnackbarComponent],
   templateUrl: './lista-produtos.component.html',
   styleUrl: './lista-produtos.component.scss'
 })
@@ -16,10 +17,19 @@ export class ListaProdutosComponent {
   produtos = produtos;
   produtosKeys = Object.keys(produtos);
 
+  snackbarMessage: string = '';
+  showSnackbar: boolean = false;
+
   constructor(private cartService: CartService) {}
 
   adicionarAoCarrinho(produto: any) {
     this.cartService.addToCart(produto);
+    this.snackbarMessage = 'Produto adicionado ao carrinho';
+    this.showSnackbar = true;
+
+    setTimeout(() => {
+      this.showSnackbar = false;
+    }, 3000);
   }
 
 }
