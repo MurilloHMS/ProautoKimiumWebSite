d5e5cededecce3341fa3b3372aefae4d8036f497
MurilloHMS <murillohenriquemelo@outlook.com>
2025-02-17 12:09:07 -0300

    incluido componente dos serviços e melhorado responsividade

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { ProdutosComponent } from "../../Components/produtos/produtos.component";
+import { ServicosComponent } from "../../Components/servicos/servicos.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProdutosComponent],
+  imports: [ProdutosComponent, ServicosComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
