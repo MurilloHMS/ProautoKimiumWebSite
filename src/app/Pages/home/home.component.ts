9b975f1ac6819f8f4696dd16b30764bff9c29915
MurilloHMS <murillohenriquemelo@outlook.com>
2025-02-27 10:43:27 -0300

    layout: incluido motivos para contratar a proauto kimium

--- src/app/Pages/home/home.component.ts
+++ src/app/Pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ProdutosComponent } from "../../Components/produtos/produtos.component";
 import { ServicosComponent } from "../../Components/servicos/servicos.component";
+import { TimelineComponent } from "../../Components/home/timeline/timeline.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProdutosComponent, ServicosComponent],
+  imports: [ProdutosComponent, ServicosComponent, TimelineComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
