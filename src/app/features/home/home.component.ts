ec80dc8d696a3f09c283538e6c9ef672ea950470
MurilloHMS <murillohenriquemelo@outlook.com>
2025-04-22 15:01:31 -0300

    fix: corrigido componentes

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,9 +1,5 @@
 import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-import { ProdutosComponent } from "../../Components/produtos/produtos.component";
-import { ServicosComponent } from "../../Components/servicos/servicos.component";
-import { TimelineComponent } from "../../Components/home/timeline/timeline.component";
 import { CarouselComponent } from "../../Components/home/carousel/carousel.component";
-import { ProductsPerDepartmentComponent } from "../../Components/home/products-per-department/products-per-department.component";
 import { HeroComponent } from "./components/hero/hero.component";
 import { ConsultoriaComponent } from "./components/consultoria/consultoria.component";
 import { ProcessoConsultoriaComponent } from "./components/processo-consultoria/processo-consultoria.component";
@@ -13,7 +9,7 @@ import { ServicosEProdutosComponent } from "./components/servicos-eprodutos/serv
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [ProdutosComponent, ServicosComponent, TimelineComponent, CarouselComponent, ProductsPerDepartmentComponent, HeroComponent, ConsultoriaComponent, ProcessoConsultoriaComponent, MotivosContratarComponent, ServicosEProdutosComponent],
+  imports: [CarouselComponent, HeroComponent, ConsultoriaComponent, ProcessoConsultoriaComponent, MotivosContratarComponent, ServicosEProdutosComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
