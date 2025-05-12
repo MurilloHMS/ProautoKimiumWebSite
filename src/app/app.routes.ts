5ccd9897af8d94328b657f167d93705a16a36bcd
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-05-12 17:27:01 -0300

    alterado rota de clientes para equipe

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,17 +3,17 @@ import { NgModule } from '@angular/core';
 import { NotFoundComponent } from './Components/not-found/not-found.component';
 import { CarrinhoComponent } from './features/carrinho/carrinho.component';
 import { CasesSucessoComponent } from './features/cases-sucesso/cases-sucesso.component';
-import { ClientesComponent } from './features/clientes/clientes.component';
 import { ContatoComponent } from './features/contato/contato.component';
 import { EmpresaComponent } from './features/empresa/empresa.component';
 import { HomeComponent } from './features/home/home.component';
 import { ListaProdutosComponent } from './features/lista-produtos/lista-produtos.component';
 import { LoginComponent } from './features/login/login.component';
+import { NossaEquipeComponent } from './features/nossa-equipe/nossa-equipe.component';
 
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, pathMatch: 'full'},
-    { path: 'clientes', component: ClientesComponent, pathMatch: 'full'},
+    { path: 'nossaEquipe', component: NossaEquipeComponent, pathMatch: 'full'},
     { path: 'empresa', component: EmpresaComponent, pathMatch: 'full'},
     { path: 'contato', component: ContatoComponent, pathMatch: 'full'},
     { path: 'carrinho', component: CarrinhoComponent, pathMatch: 'full'},
