64e7e2f851c00045c166a5f50ca7ea1a267c7b90
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-08-12 17:07:50 -0300

    layout(home): include servicos ofertados component layout

---
+++ src/app/features/home/components/servicos-ofertados/servicos-ofertados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-servicos-ofertados',
+  standalone: true,
+  imports: [],
+  templateUrl: './servicos-ofertados.component.html',
+  styleUrl: './servicos-ofertados.component.scss'
+})
+export class ServicosOfertadosComponent {
+
+}
