fd03f67f0b21193203699b8376b367df5d57da23
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-08-19 15:01:52 -0300

    feat(authentication): include authentication on website

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,19 @@ import { provideRouter } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { routes } from './app.routes';
+import { HTTP_INTERCEPTORS, provideHttpClient, withInterceptors, withInterceptorsFromDi } from '@angular/common/http';
+import { AuthInterceptor } from './Core/interceptors/auth-interceptor';
+import { AuthService } from './Core/services/auth.service';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideAnimationsAsync()
+    provideAnimationsAsync(),
+     provideHttpClient(withInterceptorsFromDi()),
+     {
+      provide : HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+     }
   ]
 };
