c219f3059ebddf52b24420939693c33f5d0d7f3e
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-08-19 10:35:08 -0300

    feat(duvidas): include duvidas component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
 import { CarrinhoComponent } from './components/carrinho/carrinho.component';
 import { CasesSucessoComponent } from './components/cases-sucesso/cases-sucesso.component';
-import { ContatoComponent } from './components/contato/contato.component';
 import { EmpresaComponent } from './components/empresa/empresa.component';
 import { HomeComponent } from './components/home/home.component';
 import { ListaProdutosComponent } from './components/lista-produtos/lista-produtos.component';
@@ -11,13 +10,13 @@ import { LoginComponent } from './components/login/login.component';
 import { NossaEquipeComponent } from './components/nossa-equipe/nossa-equipe.component';
 import { InstitucionalComponent } from './components/institucional/institucional.component';
 import { SalesCertificatesComponent } from './components/sales-certificates/sales-certificates.component';
+import { FaqComponent } from './components/faq/faq.component';
 
 
 export const routes: Routes = [
     { path: '', component: HomeComponent, pathMatch: 'full'},
     { path: 'nossaEquipe', component: NossaEquipeComponent, pathMatch: 'full'},
     { path: 'about-us', component: EmpresaComponent, pathMatch: 'full'},
-    { path: 'contato', component: ContatoComponent, pathMatch: 'full'},
     { path: 'carrinho', component: CarrinhoComponent, pathMatch: 'full'},
     { path: 'login', component: LoginComponent, pathMatch: 'full'},
     { path: 'produtos/lista', component: ListaProdutosComponent, pathMatch: 'full'},
@@ -25,6 +24,7 @@ export const routes: Routes = [
     { path: '404', component: NotFoundComponent},
     { path: 'privacy-policy', component: InstitucionalComponent, pathMatch: 'full'},
     { path: 'sales/documents/certificates', component: SalesCertificatesComponent, pathMatch: 'full'},
+    { path: 'faq', component: FaqComponent, pathMatch: 'full'},
     { path: '**', redirectTo: '404'}
 ];
 
