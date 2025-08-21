a02bbc80354c34439988897ab875a356b6fcccd4
MurilloHMS <Murillohenriquemelo@outlook.com>
2025-08-21 11:12:20 -0300

    chore: removing pages not used

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,6 @@ import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { NotFoundComponent } from './shared/not-found/not-found.component';
 import { CarrinhoComponent } from './components/carrinho/carrinho.component';
-import { CasesSucessoComponent } from './components/cases-sucesso/cases-sucesso.component';
 import { EmpresaComponent } from './components/empresa/empresa.component';
 import { HomeComponent } from './components/home/home.component';
 import { ListaProdutosComponent } from './components/lista-produtos/lista-produtos.component';
@@ -20,7 +19,6 @@ export const routes: Routes = [
     { path: 'carrinho', component: CarrinhoComponent, pathMatch: 'full'},
     { path: 'login', component: LoginComponent, pathMatch: 'full'},
     { path: 'produtos/lista', component: ListaProdutosComponent, pathMatch: 'full'},
-    { path:  'cases-sucesso', component: CasesSucessoComponent, pathMatch: 'full'},
     { path: '404', component: NotFoundComponent},
     { path: 'privacy-policy', component: InstitucionalComponent, pathMatch: 'full'},
     { path: 'sales/documents/certificates', component: SalesCertificatesComponent, pathMatch: 'full'},
